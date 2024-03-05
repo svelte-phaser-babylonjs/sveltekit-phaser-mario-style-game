a45394d28f65af47222d647cf49b0d5b9f73a934
Navid Dezashibi <knavels.official@gmail.com>
2024-03-05 11:20:13 +0400

    Initial commit

---
+++ src/lib/components/Game/Phaser.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import Phaser from 'phaser';
+
+	import { onMount } from 'svelte';
+	import Hud from './Hud.svelte';
+	import { config } from '$lib/phaser';
+
+	let gameContainer: HTMLCanvasElement;
+
+	onMount(async () => {
+		config.canvas = gameContainer;
+		const game = new Phaser.Game(config);
+
+		window.addEventListener('resize', () => {
+			game.scale.resize(window.innerWidth, window.innerHeight);
+		});
+	});
+</script>
+
+<div>
+	<canvas bind:this={gameContainer} width={window.innerWidth} height={window.innerHeight} />
+</div>
+
+<Hud />
